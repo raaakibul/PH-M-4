c765e50685db7ee8cf9451556082949e2d0bed6f
rakibul-islam <raakibul.uap@gmail.com>
2024-07-26 21:25:31 +0600

    string boolean

--- Module-16/math.js
+++ Module-16/math.js
@@ -5,6 +5,12 @@ number2 = 459
 console.log(number1+number2);
 
 // variable
-var names = "ABC"
+var names = "ABC";
 console.log(names);
 
+// string, boolean
+var letter = 'abcdefghijklmnopqrst';
+console.log(letter);
+
+var serious = true
+console.log(serious); 
