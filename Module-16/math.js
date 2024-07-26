9dc8f74e0a65fdb5e2135c639cafaa525feb8225
rakibul-islam <raakibul.uap@gmail.com>
2024-07-26 21:17:30 +0600

    math js

--- Module-16/math.js
+++ Module-16/math.js
@@ -1,4 +1,10 @@
-// number, integer, float, string
+// number, integer, float, double 
+// variable 
 number1 = 456
 number2 = 459
 console.log(number1+number2);
+
+// variable
+var names = "ABC"
+console.log(names);
+
