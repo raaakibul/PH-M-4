f4711d668151428880d7b43be3baf835ca3eafe2
rakibul-islam <raakibul.uap@gmail.com>
2024-08-04 21:48:49 +0600

    Pay amount commented

--- Module-17/if-else.js
+++ Module-17/if-else.js
@@ -8,11 +8,13 @@ else{
 
 const price = 4000;
 if(price >=5000){
+    // 10% discount
     const discount = price *10/100;
     const payAmount = price -discount;
     console.log(payAmount);
 }
 else if(price> 2500){
+    // 5% discount
     const discount = price *5 /100;
     const payAmount = price - discount
     console.log(payAmount);
