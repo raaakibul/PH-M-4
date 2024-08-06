d3b68e53ece3f2f2a798cbd21196a3ffe441a749
rakibul-islam <raakibul.uap@gmail.com>
2024-08-06 21:36:24 +0600

    Array

--- Module-17/Array.js
+++ Module-17/Array.js
@@ -1,6 +1,12 @@
 const numbers = [12,22,23,24,25,26,27,28];
 console.log(numbers);
 
+console.log(numbers[1]);
+numbers.shift(12);
+console.log(numbers);
+numbers.push(29);
+console.log(numbers);
+
 for (number in numbers) {
     console.log(number);
 }
