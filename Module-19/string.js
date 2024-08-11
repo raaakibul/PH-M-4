ca4a54c053bf74d89a3dfd12ad8d9a17a2e14042
rakibul-islam <raakibul.uap@gmail.com>
2024-08-11 21:49:01 +0600

    string operation

--- Module-19/string.js
+++ Module-19/string.js
@@ -3,3 +3,10 @@ console.log(country);
 console.log(country.length);
 console.log(country.toUpperCase());
 console.log(country.toLowerCase());
+
+const address = "Dhaka";
+const part = address.slice(2,4)
+console.log(part);
+
+const sentence = "He lives in a country name Bangladesh";
+console.log(sentence.split(" "));
