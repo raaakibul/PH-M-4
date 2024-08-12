04a90662581ff27d02d1c6be478125b5f74e372e
rakibul-islam <raakibul.uap@gmail.com>
2024-08-12 23:11:56 +0600

    object

---
+++ Module-19/object.js
@@ -0,0 +1,13 @@
+const age = 21;
+console.log(age);
+const isPassed = true;
+const subjects = ['Bangla', 'English', 'Math', 'Physics']
+console.log(subjects);
+
+// object 
+const bottle = {
+    brand:'Watt',
+    price: 12,
+    color:'White',
+    isClean:false,
+}
