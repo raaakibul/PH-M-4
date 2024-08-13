0c6d3bd05aa4c68d3db4e931506e102ca45d1367
rakibul-islam <raakibul.uap@gmail.com>
2024-08-13 23:33:52 +0600

    nested Object

---
+++ Module-19/nested-object.js
@@ -0,0 +1,10 @@
+const college = {
+    name:'VNC',
+    class:['11','12'],
+    events: ['olympiad', 'Science fair'],
+    unique:{
+        color:'blue'
+    }
+}
+console.log(college);
+console.log(college.unique);
