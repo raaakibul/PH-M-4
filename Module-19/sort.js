9447139a4d04e549414d42d8acd5a1f4dbaea9f5
rakibul-islam <raakibul.uap@gmail.com>
2024-08-18 23:06:03 +0600

    sorted javascript

--- Module-19/sort.js
+++ Module-19/sort.js
@@ -1,3 +1,8 @@
 const persons = ['rakib', 'nokib', 'sakib','akib'];
 const sortedPersons = persons.sort();
 console.log(sortedPersons);
+
+const numbers = [4,7,12,2,8,3,1];
+
+const asc_numbers = numbers.sort();
+console.log(asc_numbers);
