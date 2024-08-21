623c7111752325487a00b0b7243c342686ebe546
rakibul-islam <raakibul.uap@gmail.com>
2024-08-21 21:54:28 +0600

    array function code added

--- Module-20/function-array.js
+++ Module-20/function-array.js
@@ -1,6 +1,11 @@
-function sumofNumbers(number){
+function sumofNumbers(numbers){
+    let sum = 0;
+    for(const number of numbers){
+        sum = sum + number;
+    }
+    return sum;
 
 } 
-
-const sum = sumofNumbers();
+const nums = [45,56,78,78];
+const sum = sumofNumbers(nums);
 console.log('Sum of numbers: ', sum);
