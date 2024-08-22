b23ad356c1d0bd5bf8dce85d5c1372ccb8254fd4
rakibul-islam <raakibul.uap@gmail.com>
2024-08-23 00:08:40 +0600

    even number array

---
+++ Module-20/even.js
@@ -0,0 +1,15 @@
+function evenNumber(numbers){
+    const evens = [];
+    for(const number of numbers){
+        if(number %2 === 0){
+            console.log(number);
+            evens.push(number);
+            
+        }
+    }
+    return evens;
+}
+
+const numbers = [5,8,91,24]
+const evens = evenNumbers(numbers)
+console.log('Even numbers are :', evens);
