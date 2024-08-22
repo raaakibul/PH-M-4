1d0ae1ab918a376f57ecbb1dbbf0df6953f6d872
rakibul-islam <raakibul.uap@gmail.com>
2024-08-23 00:14:57 +0600

    sum of even number from an array

--- Module-20/even.js
+++ Module-20/even.js
@@ -13,3 +13,17 @@ function evenNumber(numbers){
 const numbers = [5,8,91,24]
 const evens = evenNumber(numbers)
 console.log('Even numbers are :', evens);
+
+function sumofEven(numbers){
+    let sum = 0;
+    for(const number of numbers){
+        if(number % 2 === 0){
+            console.log(number);
+            sum = sum + number;
+        }
+    }
+    return sum;
+}
+
+const sum = sumofEven(numbers);
+console.log('Sum of even numbers: ' + sum);
