521a1038fa7152bc3556f3b7f8f9dedb08c4f0da
rakibul-islam <raakibul.uap@gmail.com>
2024-08-24 23:48:43 +0600

    Mile to kilo

--- Module-21/convert.js
+++ Module-21/convert.js
@@ -14,3 +14,11 @@ const feet2 = inchToFeet2(75);
 console.log(feet2);
 const height = inchToFeet(68);
 console.log(height);
+
+function mileTokilometer(mile) {
+    const kilo = mile * 1.60934;
+    return kilo;
+}
+
+const kiloresult = mileTokilometer(36);
+console.log("Mile to Kilo:",kiloresult);
