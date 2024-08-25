e3352723b4ac6de50f601cb8e011655fa57c93fb
rakibul-islam <raakibul.uap@gmail.com>
2024-08-25 23:22:50 +0600

    Leap year

--- Module-21/leap-year.js
+++ Module-21/leap-year.js
@@ -10,5 +10,20 @@ function isLeapYear(year) {
     }
 }
 
-const year = isLeapYear(2014);
+const year = isLeapYear(2000);
 console.log(year);
+
+function isLeapYear2(year){
+    if(year%100 != 0 && year%4===0){
+        return true;
+    }
+    else if(year % 100 ===0 && year %400 ===0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const year2 = isLeapYear2(2100);
+console.log(year2);
