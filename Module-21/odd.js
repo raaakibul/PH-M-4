f5596da528c71bbc8208c811c6dd2170840f766f
rakibul-islam <raakibul.uap@gmail.com>
2024-08-25 23:33:53 +0600

    odd number

--- Module-21/odd.js
+++ Module-21/odd.js
@@ -1,11 +1,23 @@
 function oddAvg(numbers){
+    const odds = [];
     for(const number of numbers){
         if(number % 2 !=0){
-            console.log(number);
+            // console.log(number);
+            odds.push(number)
         }
     }
+    console.log(odds);
+    let sum = 0;
+    for (const number of numbers){
+        sum += number;
+    }
+    const count = odds.length;
+    console.log("Sum of the odd numbers:",sum);
+    const avg = sum / count;
+    // console.log("Average of the odd numbers:",avg);
+    return avg;
 
 }
-const numbers = [41,42,65,78,45,81];
+const numbers = [41,42,65,78,45,81,77,54,785];
 const result = oddAvg(numbers);
 console.log("Average of the odd Numbers:",result);
