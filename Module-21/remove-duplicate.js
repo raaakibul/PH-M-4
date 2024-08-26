8fab8802e5df622e89ede5a33eb0f7851030e786
rakibul-islam <raakibul.uap@gmail.com>
2024-08-26 22:13:53 +0600

    no duplicate

--- Module-21/remove-duplicate.js
+++ Module-21/remove-duplicate.js
@@ -3,7 +3,13 @@ const letter = ['a', 'c', 'd', 'e', 'f', 'g', 'h', 'a', 'b', 'b', 'c', 'd']
 const numbers = [21,25,25,45,45,78,78,98,98]
 
 function noDuplicates(array){
-    
+    const unique = [];
+    for(const item of array){
+        if(unique.includes(item) === false){
+            unique.push(item)
+        }       
+    }
+    return unique;
 }
 
 const result = noDuplicates(numbers)
