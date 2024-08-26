e73436c6fe7b944611022a79ea4fd6f2b1451da5
rakibul-islam <raakibul.uap@gmail.com>
2024-08-26 22:11:06 +0600

    no duplicate

---
+++ Module-21/remove-duplicate.js
@@ -0,0 +1,10 @@
+const letter = ['a', 'c', 'd', 'e', 'f', 'g', 'h', 'a', 'b', 'b', 'c', 'd']
+
+const numbers = [21,25,25,45,45,78,78,98,98]
+
+function noDuplicates(array){
+    
+}
+
+const result = noDuplicates(numbers)
+console.log(result);
