b4eaef3a7d39f73fb3a98b1d5578f2cac0c078c4
rakibul-islam <raakibul.uap@gmail.com>
2024-08-26 22:20:38 +0600

    math function

--- Module-21/math-function.js
+++ Module-21/math-function.js
@@ -1,7 +1,14 @@
-const Math = {
-    min: function min(a,b){
-        return b;
-    }
-}
-console.log(Math.min);
-// const min = Math.min()
+// const Math = {
+//     min: function min(a,b){
+//         return b;
+//     }
+// }
+// console.log(Math.min);
+const min = Math.min(41,45,85,5,45,78)
+const max = Math.max(41,45,85,5,45,78)
+console.log("Minimum :",min);
+console.log("Maximum :",max);
+
+console.log(Math.PI);
+console.log(Math.sqrt(54654));
+console.log(Math.abs(-3.125));
