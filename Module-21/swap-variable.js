0609ec370fcb2ccc4e48002ba673f354d8922ffd
rakibul-islam <raakibul.uap@gmail.com>
2024-08-28 00:02:05 +0600

    swap the variable

--- Module-21/swap-variable.js
+++ Module-21/swap-variable.js
@@ -4,8 +4,16 @@
 
 let a = 2;
 let b = 3;
-
+console.log(a,b);
 const temp = a;
 a = b;
 b = temp;
 console.log(a,b);
+
+// 
+let x = 5;
+let y =7;
+console.log(x,y);
+
+[x,y] = [y,x];
+console.log(x,y);
