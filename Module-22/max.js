bf48814aff4d26fb97fb2a4d248f8e416a17ebf8
rakibul-islam <raakibul.uap@gmail.com>
2024-08-28 00:06:54 +0600

    maximum number

---
+++ Module-22/max.js
@@ -0,0 +1,22 @@
+const name1 = 56;
+const name2 = 65;
+
+if(name1> name2){
+    console.log(name1);
+}
+else{
+    console.log(name2);
+}
+
+// use function
+function getMax(num1,num2) {
+    if(name1> name2){
+        console.log(name1);
+    }
+    else{
+        console.log(name2);
+    }
+}
+
+const max = getMax(45,89);
+console.log(max);
