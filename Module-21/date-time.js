7b25e81ccb3b36f2fd8e362d49981d31bad045f1
rakibul-islam <raakibul.uap@gmail.com>
2024-08-27 23:46:07 +0600

    Date and time

--- Module-21/date-time.js
+++ Module-21/date-time.js
@@ -4,4 +4,7 @@
 console.log("Ddate and time");
 const today = new Date();
 const date = new Date('2062-10-09');
-console.log(date);
+console.log(date.getMonth());
+console.log(date.getDay());
+
+const specificDate = new Date(2091,1,26);
