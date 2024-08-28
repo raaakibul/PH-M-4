24833ac4e632bfebf05d350303502e113bcbf3b1
rakibul-islam <raakibul.uap@gmail.com>
2024-08-28 21:37:50 +0600

    calculate wood code added

---
+++ Module-22/calculate-wood.js
@@ -0,0 +1,23 @@
+/***
+ * calculate the wood
+ * chair --> 3cft
+ * table ---> 10cft
+ * bed --> 50 cft
+ */
+
+function calculateWoods(chair, table, bed) {
+    const perChair = 3;
+    const perTable = 10;
+    const perBed = 50;
+
+    const allChair = chair * perChair;
+    const allTable = table * perTable;
+    const allBed = bed * perBed;
+
+    const totalWood = allChair + allTable + allBed;
+
+    return totalWood;
+}
+
+const result = calculateWoods(0,0,1);
+console.log("Woods calculated:", result);
