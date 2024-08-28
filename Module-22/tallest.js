274f4762a632c5b464e981fa534e801da4d9cf24
rakibul-islam <raakibul.uap@gmail.com>
2024-08-28 21:29:55 +0600

    heights code added

--- Module-22/tallest.js
+++ Module-22/tallest.js
@@ -1,2 +1,15 @@
 const heights = [65,66,78,25,56,50,45]
-console.log(heights);
+// console.log(heights);
+
+function getMaxHeight(numbers){
+    let max = heights[0];
+    for(const number of numbers){
+        if(number > max){
+            max = number;
+        }
+    }
+    return max;
+}
+
+const result = getMaxHeight(heights);
+console.log("Max Height: " + result);
