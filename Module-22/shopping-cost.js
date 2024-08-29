48cf14ea7302da06b9fec345b01327d52aac623d
rakibul-islam <raakibul.uap@gmail.com>
2024-08-29 22:24:49 +0600

    shopping code

--- Module-22/shopping-cost.js
+++ Module-22/shopping-cost.js
@@ -1,15 +1,18 @@
 const products = [
-    {name: 'Shampoo', price: 300},
-    {name: 'Brush', price: 100},
-    {name: 'Shirt', price: 500},
-    {name: 'Pant', price:700}
+    {name: 'Shampoo', price: 300, quantity: 1},
+    {name: 'Brush', price: 100, quantity: 5},
+    {name: 'Shirt', price: 500, quantity: 2},
+    {name: 'Pant', price:700, quantity: 3}
 ]
 
 function getShopping(products){
+    let sum = 0;
     for(const product of products){
-        console.log(product);
+        // console.log(product);
+        sum = sum + product.price[i];
     }
+    return sum;
 }
 
 const total = getShopping(products);
-console.log("Total : "+ total);
+console.log("Total Cost: "+ total);
