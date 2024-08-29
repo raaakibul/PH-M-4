12325fd38f4fb5e46ae496b9ac17406249aa88ce
rakibul-islam <raakibul.uap@gmail.com>
2024-08-29 21:56:51 +0600

    minimum code  added

--- Module-22/minimum.js
+++ Module-22/minimum.js
@@ -1,6 +1,14 @@
 const price = [20000,15000,45000,100000,120000,145693]
 
-function getMinimum(price){
+function getMinimum(numbers){
+    let min = numbers[0];
+    for(const number of numbers){
+        // console.log(number);
+        if(number < min){
+            min = number;
+        }
+    }
+    return min;
 
 }
 const result = getMinimum(price);
