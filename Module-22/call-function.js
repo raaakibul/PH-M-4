ffd79b09cb8253a612522bca9aa46aef293ddd91
rakibul-islam <raakibul.uap@gmail.com>
2024-08-30 21:18:47 +0600

    Simple calculator to call function inside a function

--- Module-22/call-function.js
+++ Module-22/call-function.js
@@ -5,5 +5,37 @@
 function add(num1, num2){
     return num1 + num2;
 }
-const addition = add(1, 2);
-console.log(addition);
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+function divide(num1, num2){
+    return num1 / num2;
+}
+function multiply(num1, num2){
+    return num1 * num2;
+}
+function calculator(a,b,operator){
+    if(operator === 'add'){
+        const result = add(a,b);
+        return result;
+    }
+    else if(operator === 'subtract'){
+        const result = subtract(a,b);
+        return result;
+    }
+    else if(operator === 'divide'){
+        const result = divide(a,b);
+        return result;
+    }
+    else if(operator === 'multiply'){
+        const result = multiply(a,b);
+        return result;
+    }
+    else{
+        console.log("Invalid operation");
+    }
+}
+
+const operation = calculator(45, 100, 'divide');
+console.log(operation);
