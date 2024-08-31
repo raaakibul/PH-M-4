f5b8f446bf04863bbbb07c12ab6196136aa0a502
rakibul-islam <raakibul.uap@gmail.com>
2024-08-31 23:31:33 +0600

    Handle unexpected function input parameter error

--- Module-22/function-error.js
+++ Module-22/function-error.js
@@ -7,3 +7,13 @@ function multiply(num1, num2) {
 
 const result = multiply(1, 2);
 console.log(result);
+const result2 = multiply(5,'seven');
+console.log(result2);
+
+
+function fullName(first, second) {
+    const full = first + ' ' + second;
+    return full;
+}
+const full = fullName('Rakibul', 'Islam');
+console.log(full);
