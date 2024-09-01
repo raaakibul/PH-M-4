bfd46cc9773e9420fa082f2faaa1e92b5a869750
rakibul-islam <raakibul.uap@gmail.com>
2024-09-01 21:47:11 +0600

    (optional) Explore what you can do with JavaScript Object

---
+++ Module-23/object.js
@@ -0,0 +1,13 @@
+const student = {
+    name: 'Student1',
+    id: 121,
+    address: 'Dhaka',
+    isSingle: true,
+    friends: ['Student1', 'Student2', 'Student3'],
+    car:{
+        brand: 'tesla',
+        price: 50000000,
+        model:2025
+    }
+}
+console.log(student);
