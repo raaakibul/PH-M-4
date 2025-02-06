2cd02fb88e89dc688190789d87b95ff13e7ea678
rakibul-islam <raakibul.uap@gmail.com>
2025-02-06 21:17:17 +0600

    javascript oop code

--- Java-OOP/object.js
+++ Java-OOP/object.js
@@ -5,6 +5,10 @@ var person ={
     age: 22,
     job: "Student",
     address: "Dhaka, Bangladesh",
+    getName:()=>{
+        console.log(`${person.firstName} ${person.lastName}`);
+        console.log(`age : ${person.age}, job : ${person.job}`);
+    }
 }
 
 // Accessing object properties
@@ -20,6 +24,9 @@ let computer = {
     ram: "16GB",
     storage: "512GB SSD",
     price: "1200 usd $",
+    getDetails:()=>{
+        console.log(`Brand : ${computer.brand}, Model : ${computer.model}`);
+    }
 }
 console.log(computer);
 console.log(computer.brand);
