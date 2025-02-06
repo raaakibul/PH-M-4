8f094ff9e8821dab690735165ab6fb238ed6ce1c
rakibul-islam <raakibul.uap@gmail.com>
2025-02-06 21:11:15 +0600

    javascript oop code

---
+++ Java-OOP/object.js
@@ -0,0 +1,26 @@
+// create a object
+var person ={
+    firstName: "Md. Tazri",
+    lastName: "Milon",
+    age: 22,
+    job: "Student",
+    address: "Dhaka, Bangladesh",
+}
+
+// Accessing object properties
+console.log(person);
+console.log(person.firstName);
+console.log(person.lastName);
+
+
+let computer = {
+    brand: "Asus",
+    model: "ROG",
+    processor: "Intel Core i7",
+    ram: "16GB",
+    storage: "512GB SSD",
+    price: "1200 usd $",
+}
+console.log(computer);
+console.log(computer.brand);
+console.log(computer.model);
