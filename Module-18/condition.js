d4523936a0f2fd2a4dcf89640a319130ceec0bd5
rakibul-islam <raakibul.uap@gmail.com>
2025-04-06 21:07:27 +0600

    Conditional

---
+++ Module-18/condition.js
@@ -0,0 +1,20 @@
+number1 = 25
+number2 = 30
+if (number1 > number2) {
+    console.log("Number 1 is greater than Number 2")
+}
+else if (number1 < number2) {
+    console.log("Number 1 is less than Number 2")
+}
+else if (number1 == number2) {
+    console.log("Number 1 is equal to Number 2")
+}
+else if (number1 != number2) {
+    console.log("Number 1 is not equal to Number 2")
+}
+else if (number1 >= number2) {
+    console.log("Number 1 is greater than or equal to Number 2")
+}
+else if (number1 <= number2) {
+    console.log("Number 1 is less than or equal to Number 2")
+}
